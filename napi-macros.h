d6b5fb74e98060d06aa7c45cc451ebd16356141a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-08-06 13:01:08 +0200

    Add NAPI_ARGV_UTF8_MALLOC and NAPI_UTF8_MALLOC

--- napi-macros.h
+++ napi-macros.h
@@ -113,6 +113,14 @@
     return NULL; \
   }
 
+#define NAPI_UTF8_MALLOC(name, val) \
+  size_t name##_size = 0; \
+  NAPI_STATUS_THROWS(napi_get_value_string_utf8(env, val, NULL, 0, &name##_size)); \
+  char* name = malloc((name##_size + 1) * sizeof(char)); \
+  size_t name##_len; \
+  NAPI_STATUS_THROWS(napi_get_value_string_utf8(env, val, name, name##_size + 1, &name##_len)); \
+  name[name##_size] = '\0';
+
 #define NAPI_UINT32(name, val) \
   uint32_t name; \
   if (napi_get_value_uint32(env, val, &name) != napi_ok) { \
@@ -148,6 +156,9 @@
 
 #define NAPI_ARGV_UTF8(name, size, i) \
   NAPI_UTF8(name, size, argv[i])
+
+#define NAPI_ARGV_UTF8_MALLOC(name, i) \
+  NAPI_UTF8_MALLOC(name, argv[i])
 
 #define NAPI_ARGV_UINT32(name, i) \
   NAPI_UINT32(name, argv[i])
